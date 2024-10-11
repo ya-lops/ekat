b9ad643193a1fd550237192ae243ed0f77d72022
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-10-11 15:39:09 +0300

    add blog cases preview listing

---
+++ src/data/blog.js
@@ -0,0 +1,29 @@
+const blog = [
+  {
+    slug: "blog-case-1",
+    type: "case",
+    date: "",
+    title: "Продуктовый стартап",
+    image: "/images/blog/blog-case-1.jpg",
+    description:
+      "Анонс новости на две-три строчки, чтобы здесь было заполнено текстом и выглядело хорошо. Сюда можно ставить первые 100 знаков самого кейса.",
+  },
+  {
+    slug: "blog-case-2",
+    type: "case",
+    date: "",
+    title: "Миллион упаковок за 1 месяц",
+    image: "/images/blog/blog-case-2.jpg",
+    description: "Анонс новости на одну строку",
+  },
+  {
+    slug: "blog-case-3",
+    type: "case",
+    date: "",
+    title: "Новый дизайн — новый уровень продаж",
+    image: "/images/blog/blog-case-3.jpg",
+    description: "Два слова",
+  },
+];
+
+export default blog;
