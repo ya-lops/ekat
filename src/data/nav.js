d5ef3a7f1e20f2a4a575b035432f40c0d60f0bc3
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-10-11 16:24:20 +0300

    nav and routing

---
+++ src/data/nav.js
@@ -0,0 +1,39 @@
+const nav = [
+  {
+    label: 'О компании',
+    href: '/about',
+    sublinks: [
+      {label: 'Производство', href: '/about/production'},
+      {label: 'Сертификаты', href: '/about/certificates'},
+      {label: 'Технологии', href: '/about/technologies'},
+      {label: 'Процесс работы', href: '/about/process'},
+      {label: 'Отзывы', href: '/about/reviews'},
+    ]
+  },
+  {
+    label: 'Каталог',
+    href: '/catalog',
+    sublinks: [
+      {label: 'Стаканы', href: '/catalog'},
+      {label: 'Крышки', href: '/catalog'},
+      {label: 'Ложки', href: '/catalog'},
+      {label: 'Контейнеры', href: '/catalog'},
+      {label: 'Индивидуальные решения', href: '/catalog/individual'},
+    ]
+  },
+  {
+    label: 'Блог',
+    href: '/blog',
+    sublinks: [
+      {label: 'Новости', href: '/blog/news'},
+      {label: 'Статьи', href: '/blog/articles'},
+      {label: 'Кейсы', href: '/blog/cases'},
+    ]
+  },
+  {
+    label: 'Контакты',
+    href: '/contacts',
+  }
+];
+
+export default nav;
