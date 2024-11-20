27e6c2f5bd1ee723791fbddc92e8fffa5d3af210
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-11-20 22:20:37 +0300

    using content collection

--- src/content/config.ts
+++
@@ -1,9 +0,0 @@
-// import { defineCollection } from "astro:content";
-
-// const products = defineCollection({
-//   type: "content"
-// });
-
-// export const collections = {
-//   'products': products,
-// };
